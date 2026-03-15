455e146a1e6558a88cd84190f7984389a58a30c4
Nizarrafi <64028252+Nizarrafi@users.noreply.github.com>
2026-03-16 01:44:17 +0700

    Update variables.tf

--- terraform/week1/variables.tf
+++ terraform/week1/variables.tf
@@ -11,13 +11,12 @@ variable "key_name" {
 }
 
 variable "ami_id" {
-  description = "Amazon Linux 2 AMI ID"
+  description = "Amazon Linux 2023 AMI ID"
   type        = string
-  default     = "ami-04dd8f0d80ed6323e"
+  default     = "ami-0d5f3e23c61fbb3e8"
 }
 
 variable "ssh_allowed_ip" {
   description = "IP address allowed for SSH access (CIDR format)"
   type        = string
-  default     = "103.175.48.92/32"
 }
